1998132df731f7b07d21a968383bbc3fced7ab48
ksc <76959808+ksc4305@users.noreply.github.com>
2024-10-28 15:54:30 +0900

    Add files via upload

---
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.jdbc.util;
+
+import java.sql.*;
+
+public class DbUtils {
+    public DbUtils(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            //todo connection.
+            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_1","nhn_academy_1","ubfQOVGF7nAp$$OE");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
